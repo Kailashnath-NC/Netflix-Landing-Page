b74168b88b6df5e8a29f97a88c287f306aa01ec1
KailashnathNC <114307951+KailashnathNC@users.noreply.github.com>
2023-12-26 22:40:06 +0530

    little js touch-up

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-// const questionList = document.querySelectorAll(".question-section");
-// const answerList = document.querySelectorAll(".answer-section");
-// // console.log(questionList);
-// // console.log(answerList);
-
-// function closeAllAnswers() {
-//   answerList.forEach((answer) => {
-//     answer.classList.remove("open");
-//   });
-// }
-
-// questionList.forEach((question, index) => {
-//   //   console.log(question);
-//   //   console.log(index);
-//   question.addEventListener("click", function () {
-//     console.log(question.classList);
-//     if (answerList[index].classList.contains("open")) {
-//       answerList[index].classList.remove("open");
-//     } else {
-//       closeAllAnswers();
-//       answerList[index].classList.add("open");
-//     }
-//   });
-// });
-
+// For the FAQ's opening effect
 const questionList = document.querySelectorAll(".question-section");
 const answerList = document.querySelectorAll(".answer-section");
 
@@ -46,5 +22,24 @@ questionList.forEach((question) =>
       answer.style.maxHeight = answer.scrollHeight + "px";
       plusIcon.style.transform = "rotate(45deg)";
     }
+  })
+);
+
+// For handling buttons
+const allBtns = document.querySelectorAll(".btn");
+
+allBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    const messages = [
+      "Oops! The magic behind these buttons is just an illusion.",
+      "Sorry, these buttons are on strike today!",
+      "Looks like you found the mystery buttons! Spoiler: They do nothing.",
+      "These buttons were trained by ninjas, but they still won't do anything.",
+      "Congratulations! You clicked the button, and nothing happened. Enjoy the show!",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const funMessage = messages[randomIndex];
+    alert(funMessage);
   })
 );
